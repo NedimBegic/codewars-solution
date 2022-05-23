0e7f6bc33e673c6d4db89879e8912588e257ae28
Nedim Begic <nedim.begic93@gmail.com>
2022-05-23 19:30:04 +0200

    new solutions

--- js/solution003.js
+++ js/solution003.js
@@ -1,2 +1,2 @@
 // Write a function to split a string and convert it into an array of words.
-const stringToArray = string => string.split(" ")
+const stringToArray = string => string.split(" ")po
---
+++ js/solution006 Grasshopper - Personalized Message.js
@@ -0,0 +1,4 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+
+const greet = (name, owner) => name===owner ? "Hello boss" : "Hello guest";
