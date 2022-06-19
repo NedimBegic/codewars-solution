8395c024e79bca9cdade5ed7682ccf04cc81a051
Nedim Begic <nedim.begic93@gmail.com>
2022-06-19 17:33:46 +0200

    new

---
+++ kata 8/5 without numbers !!.js
@@ -0,0 +1,4 @@
+/* Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/ */
+const unusualFive = _ => ["d","f","g","a","e"].length;
