bab13e2ffca5205e78eef771728a5fb038218ec9
Nedim Begic <nedim.begic93@gmail.com>
2022-07-20 14:02:01 +0200

    new

--- kata 8/Find the position!.js
+++ kata 8/Find the position!.js
@@ -4,7 +4,7 @@ Input :: "a"
 
 Ouput :: "Position of alphabet: 1" */
 function position(letter){
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let indexPosition = alphabet.indexOf(letter);
-    return indexPosition+1;
-  }
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let indexPosition = alphabet.indexOf(letter);
+  return `Position of alphabet: ${indexPosition+1}`
+}
