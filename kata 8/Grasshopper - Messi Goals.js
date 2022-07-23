9e7eed8b566ebbaa81c1a6405ed7411b90738a5f
Nedim Begic <nedim.begic93@gmail.com>
2022-07-23 02:24:27 +0200

    new

---
+++ kata 8/Grasshopper - Messi Goals.js
@@ -0,0 +1,16 @@
+/* Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+Create these three variables and store the appropriate values using the */
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals +championsLeagueGoals + copaDelReyGoals;
