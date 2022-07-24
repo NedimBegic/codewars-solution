600130dd1900fc81767e0c604020190850a548ee
Nedim Begic <nedim.begic93@gmail.com>
2022-07-24 23:01:29 +0200

    new

---
+++ kata 8/Apple Turnover.js
@@ -0,0 +1,9 @@
+/* Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, 
+else, return Help yourself to a honeycomb Yorkie for the glovebox. */
+function apple(x){
+    let num = +x;
+    
+     return num**2>1000 ? 
+     "It's hotter than the sun!!":
+     "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
