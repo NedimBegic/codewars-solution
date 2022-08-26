b81d092c7d9d1914ca72f44b9b8bad73451637a8
Nedim Begic <nedim.begic93@gmail.com>
2022-08-26 18:35:29 +0200

    new

---
+++ kata 8/Remove First and Last Character Part Two.js
@@ -0,0 +1,8 @@
+/* You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the
+ first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting 
+string to be empty, return an empty value. */
+const array = arr => arr.split(",").slice(1,-1).join(" ") || null 
