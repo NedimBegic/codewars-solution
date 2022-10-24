f66a3f285f78a37c140586ebe5e1116fdef9cec8
Nedim Begic <nedim.begic93@gmail.com>
2022-10-24 19:44:10 +0200

    new

---
+++ kata 8/Polish alphabet.js
@@ -0,0 +1,29 @@
+/* There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+Your task is to change the letters with diacritics:
+
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+and print out the string without the use of the Polish letters. */
+var mapping = {
+    'ą': 'a',
+    'ć': 'c',
+    'ę': 'e',
+    'ł': 'l',
+    'ń': 'n',
+    'ó': 'o',
+    'ś': 's',
+    'ź': 'z',
+    'ż': 'z',
+  };
+  
+  function correctPolishLetters(text) {
+    return text.split('').map(c => mapping[c] || c).join('');
+  }
