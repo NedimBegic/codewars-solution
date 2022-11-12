363729737f847fedcefc87ed09626b17e4c25a5d
Nedim Begic <nedim.begic93@gmail.com>
2022-11-12 16:02:16 +0100

    new

---
+++ kata 8/Grasshopper - Create the rooms.js
@@ -0,0 +1,9 @@
+/* You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all
+ of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties
+  (e.g. name, description, completed). */
+var rooms = {
+    name:{name:true,description:false,completed:true},
+    description:{name:true,description:false,completed:true},
+    completed:{name:true,description:false,completed:true},
+  
+  }
