c8960922c3e65cc8e640f56976b907d06693ead9
nedim begic <nedim.begic93@gmail.com>
2023-07-13 15:11:28 +0200

    new

---
+++ kata 7/Alternating between three values.js
@@ -0,0 +1,14 @@
+/* Alternating Among Three Values
+Suppose a variable x can have only three possible different values a, b and c, and you wish to assign to x the value other than its current one, and you wish your code to be independent of the values of a, b and c.
+
+What is the most efficient way to cycle among three values? Write a function f so that it satisfies
+
+  f(a) = b
+  f(b) = c
+  f(c) = a
+EXAMPLE
+  f( 3, { a:3, b:4, c:5 } ) => 4 */
+const f = (x, cc) =>
+  Object.values(cc).indexOf(x) === Object.values(cc).length - 1
+    ? Object.values(cc)[0]
+    : Object.values(cc)[Object.values(cc).indexOf(x) + 1];
