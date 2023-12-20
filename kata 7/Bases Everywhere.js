e834a9860d999167799652f679d7522e8e6afc96
nedim begic <nedim.begic93@gmail.com>
2023-12-20 21:47:11 +0100

    new

---
+++ kata 7/Bases Everywhere.js
@@ -0,0 +1,40 @@
+/* Uh oh, Someone at the office has dropped all these sequences on the floor and forgotten to label them with their correct bases.
+
+We have to fix this before the boss gets back or we're all going to be fired!
+
+This is what your years of coding have been leading up to, now is your time to shine!
+
+Task
+You will have to create a function which takes in a sequence of numbers in random order and you will have to return the correct base of those numbers.
+
+The base is the number of unique digits. For example, a base 10 number can have 10 unique digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 and a base 2 number (Binary) can have 2 unique digits: 0 and 1.
+
+Constraints
+The sequence will always be 10 numbers long and we know that the base is going to be between 2 and 10 inclusive so no need to worry about any letters. When sorted, the sequence is made up of consecutive numbers.
+
+Examples
+[ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ]     -->  10
+[ "1", "2", "3", "4", "5", "6", "10", "11", "12", "13" ]  -->   7
+Good luck! */
+function baseFinder(seq) {
+  let sorted = seq.sort((x, y) => x - y).map((e) => +e);
+  let base = 0;
+  let isTen = true;
+  for (let i = 1; i < sorted.length; i++) {
+    if (sorted[i] !== sorted[i - 1] + 1) {
+      isTen = false;
+    }
+  }
+  if (isTen) return 10;
+  for (let i = 1; i < seq.length; i++) {
+    if (sorted[i + 1] != sorted[i] + 1) {
+      if (sorted[0] % 10 == 1) {
+        base = sorted[i] + 2 - sorted[0];
+      } else {
+        base = sorted[i] + 1 - sorted[0];
+      }
+      break;
+    }
+  }
+  return base;
+}
